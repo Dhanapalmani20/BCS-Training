76364872f456ea8c3176c6f1f578187492c43f5a
Dhanapalmani20 <dhanapalmani15052000@gmail.com>
2025-12-19 11:00:49 +0530

    updated all

--- Day- 6/04-object-destructuring.js
+++ Day- 6/04-object-destructuring.js
@@ -12,10 +12,19 @@ const student = {
 // 1. name
 // 2. city
 // 3. second hobby - "football"
-const {name,address}=student;
-const{hobbies}=student;
-const[,h2]=hobbies;
-const {city}=address;
+// const {name,address}=student;
+// const{hobbies}=student;
+// const[,h2]=hobbies;
+// const {city}=address;
+// console.log(name);
+// console.log(city);
+// console.log(h2 );
+const {
+  name,
+  address: { city },
+  hobbies: [, h2],
+} = student;
+//const{city,}=(hobbies,address);
 console.log(name);
 console.log(city);
-console.log(h2 );
+console.log(h2);
