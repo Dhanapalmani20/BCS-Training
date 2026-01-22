ea1494fab5f8fd83683c36015ad220aeb6031bf9
Dhanapalmani20 <dhanapalmani15@gmail.com>
2026-01-22 11:19:09 +0530

    test01

--- Day-22/task.js
+++ Day-22/task.js
@@ -7,10 +7,10 @@
 // - Get all countries common name
 // - Output
 // `['China', 'India', .... 250 countries]`
-
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,flags,region,population,capital",
 )
-  .then((res) => res.json())// respones object
+  //promise
+  .then((res) => res.json()) // respones object
   .then((names) => names.map((name1) => name1.name.common))
   .then((data) => console.log(data));
