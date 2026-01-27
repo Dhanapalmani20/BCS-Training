65e4f9da45e66e6ac9d8504f838e363a74efd070
Dhanapalmani20 <dhanapalmani15@gmail.com>
2026-01-27 18:09:04 +0530

    html form

--- project/movie-app-bcs-dec-2025/src/App.jsx
+++ project/movie-app-bcs-dec-2025/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+//Component = UI + Logic
+export default function App() {
+  //Logic Start
   return (
-    <>
+    //UI Starts
+    <div className="App">
+      <User
+        name="Arjun"
+        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
+      />
+      <User
+        name="mani"
+        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
+      />
+      <User
+        name="Saravana"
+        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
+      />
+    </div>
+  );
+
+  function User({ name, img }) {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <img className="num-1" src={img} alt="" />
+        <p className="user-msg">
+          Hello,<span className="p-name"> {name}</span> 🎉🎉
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
-
-export default App
