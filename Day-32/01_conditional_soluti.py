068aea4d896c7d03d3e9569a952c37a8cecbb65d
Dhanapalmani20 <dhanapalmani15@gmail.com>
2026-02-24 11:13:00 +0530

    updated
    python

---
+++ Day-32/01_conditional_soluti.py
@@ -0,0 +1,26 @@
+# Task 1.1
+# Compare Two People’s Heights (Taller / Same Height)
+# Hint - input
+# Better - abs()
+# Expected Output -
+# Case 1:
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 163
+# Luffy is taller than Zoro by 10cm
+
+# Case 2:
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 185
+# Zoro is taller than Luffy by 12cm
+
+
+# Case 3
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 173
+# Luffy and Zoro are of same height
